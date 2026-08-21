a172cb7fc303b6f61e7a229807c9afa701e0fb64
Sergey <skauzerlfc@gmail.com>
2026-08-21 11:22:17 +0300

    add pwa install

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,21 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import "@khmyznikov/pwa-install";
 
 export function RootLayout() {
-  return <Outlet />
+  return (
+    <>
+      <pwa-install
+        use-local-storage
+        // install-description="Custom call to install text"
+        disable-screenshots
+        disable-screenshots-apple
+        disable-screenshots-chrome
+        disable-android-fallback
+        name="Food detector"
+        description="Food detector"
+        icon="/icon.png"
+      ></pwa-install>
+      <Outlet />
+    </>
+  );
 }
